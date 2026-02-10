746a90e9417ab2430a725c8c70c336c09c6a357a
Tian <xiatian1996cd@gmail.com>
2026-02-10 18:15:36 +0800

    Finished the constructor of class Node

---
+++ decision_tree.py
@@ -0,0 +1,9 @@
+class Node:
+  def __init__(self):
+    self.children = {} # Key: feature value, Value: child node
+    self.feature = None # Feature used for splitting at this node
+    self.label = None # if the node is a leaf, this stores the predicted class label
+  
+
+  
+
