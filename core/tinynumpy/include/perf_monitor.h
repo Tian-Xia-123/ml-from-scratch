56f65d5f89fce9567030affb0dde0ebbf9fa9726
tian <xiatian1996cd@gmail.com>
2026-02-16 17:36:39 +0800

    feat: add perf_monitor.h public interface

---
+++ core/tinynumpy/include/perf_monitor.h
@@ -0,0 +1,16 @@
+#ifndef PERF_MONITOR_H
+#define PERF_MONITOR_H
+
+// Initializes the perf event
+int setup_perf_event(int event);
+
+// Starts the perf event
+void start_perf_event(int fd);
+
+// Stops the perf event
+void stop_perf_event(int fd);
+
+// Reads the value of the perf event
+long long read_perf_event(int fd);
+
+#endif
